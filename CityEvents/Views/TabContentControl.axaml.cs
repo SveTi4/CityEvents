fb9475377c415923ee8223f96660b4658cf617a8
SveTi4 <studymail2.7.53@gmail.com>
2023-03-05 21:45:05 +0700

    Move content template in usercontrol

---
+++ CityEvents/Views/TabContentControl.axaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.ObjectModel;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using CityEvents.Models;
+
+namespace CityEvents.Views;
+
+public partial class TabContentControl : UserControl
+{
+    public static readonly StyledProperty<ReadOnlyObservableCollection<CityEvent>> EventsProperty =
+        AvaloniaProperty.Register<TabContentControl, ReadOnlyObservableCollection<CityEvent>>(nameof(Events));
+
+    public ReadOnlyObservableCollection<CityEvent> Events
+    {
+        get => GetValue(EventsProperty);
+        set => SetValue(EventsProperty, value);
+    }
+
+    public TabContentControl()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
