47b51607f68d395740713909c94336afc7401f8b
SveTi4 <studymail2.7.53@gmail.com>
2023-03-05 19:19:48 +0700

    Add img converter, create tabs and weite events

---
+++ CityEvents/Models/CityEvent.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace CityEvents.Models
+{
+    public class CityEvent
+    {
+        private string _imgLink = "";
+        private string _title = "";
+        private string _date = "";
+        private string _description = "";
+        private float _price = 0;
+        private ushort _maxPrintLength = 135;
+
+        public string ImgLink
+        {
+            get => _imgLink;
+            set => _imgLink = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        public string Title
+        {
+            get => _title;
+            set => _title = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        public string Date
+        {
+            get => _date;
+            set => _date = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        public string Description
+        {
+            get
+            {
+                if (_description.Length > _maxPrintLength)
+                    return _description.Substring(0, _maxPrintLength) + "...";
+                
+                return _description;
+            }
+            set => _description = value ?? throw new ArgumentNullException(nameof(value));
+        }
+        
+        public string FullDescription
+        {
+            get => _description;
+            set => _description = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        public float Price
+        {
+            get => _price;
+            set => _price = value;
+        }
+    }
+}
---
+++ CityEvents/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using CityEvents.Models;
+using ReactiveUI;
+
+namespace CityEvents.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private CityEvent[] _TabEvents;
+        // private string _TabTitle;
+        
+        public MainWindowViewModel()
+        {
+            TabEvents = new CityEvent[]
+            {
+                new CityEvent(){Title= "Event title 1", Date = "12.04.2023", Description = " Lorem ipsum dolor sit " +
+                    "amet, consectetur adipiscing elit. Duis in luctus urna. Morbi sit amet metus pellentesque, " +
+                    "rhoncus urna sed, varius lacus. Interdum et malesuada fames ac ante ipsum primis in faucibus. " +
+                    "Fusce tincidunt turpis odio, id condimentum odio convallis sed. Nulla facilisi. Aenean feugiat " +
+                    "augue sit amet massa consectetur, nec tincidunt quam efficitur. Fusce aliquam urna dolor, eget " +
+                    "dapibus nunc posuere non. Curabitur ipsum velit, sodales at scelerisque id, commodo vitae lorem. " +
+                    "Curabitur bibendum ut elit vel egestas. Donec venenatis elit et sapien consequat faucibus. Duis " +
+                    "gravida enim commodo pellentesque rutrum. Cras efficitur sapien augue, ut convallis odio finibus " +
+                    "dignissim. Aliquam vitae leo elementum, hendrerit sem at, auctor elit. Nullam consequat tortor et " +
+                    "erat tempor volutpat. Vivamus feugiat tortor vel urna gravida viverra. Curabitur sodales a arcu " +
+                    "sit amet fermentum. ", Price = 0},
+                new CityEvent(){Title= "Event title 2", Date = "08.03.2023",  Description = "Description 2", Price = 200},
+                new CityEvent(){Title= "Event title 3", Date = "10.05.2023",  Description = "Description 3", Price = 100},
+                new CityEvent(){Title= "Event title 4", Date = "01.06.2023",  Description = "Description 4", Price = 0},
+            };
+        }
+
+        public CityEvent[] TabEvents { get => _TabEvents; set => this.RaiseAndSetIfChanged(ref _TabEvents, value); }
+        // public string TabTitle { get => _TabTitle; set => this.RaiseAndSetIfChanged(ref _TabTitle, value); }
+    }
+}
+
