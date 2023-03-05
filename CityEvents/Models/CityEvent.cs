1c07a43c219ce8541d5d1eae6c6447d323a1eaf8
SveTi4 <studymail2.7.53@gmail.com>
2023-03-05 21:48:10 +0700

    Add category in cityevent

--- CityEvents/Models/CityEvent.cs
+++ CityEvents/Models/CityEvent.cs
@@ -1,32 +1,34 @@
 ﻿using System;
+using DynamicData.Binding;
 
 namespace CityEvents.Models
 {
-    public class CityEvent
+    public class CityEvent: AbstractNotifyPropertyChanged
     {
         private string _imgLink = "";
         private string _title = "";
         private string _date = "";
         private string _description = "";
         private float _price = 0;
+        private Category _category;
         private ushort _maxPrintLength = 135;
 
         public string ImgLink
         {
             get => _imgLink;
-            set => _imgLink = value ?? throw new ArgumentNullException(nameof(value));
+            set => SetAndRaise(ref _imgLink, value);
         }
 
         public string Title
         {
             get => _title;
-            set => _title = value ?? throw new ArgumentNullException(nameof(value));
+            set => SetAndRaise(ref _title, value);
         }
 
         public string Date
         {
             get => _date;
-            set => _date = value ?? throw new ArgumentNullException(nameof(value));
+            set => SetAndRaise(ref _date, value);
         }
 
         public string Description
@@ -38,19 +40,24 @@ namespace CityEvents.Models
                 
                 return _description;
             }
-            set => _description = value ?? throw new ArgumentNullException(nameof(value));
+            set => SetAndRaise(ref _description, value);
         }
         
         public string FullDescription
         {
             get => _description;
-            set => _description = value ?? throw new ArgumentNullException(nameof(value));
+            set => SetAndRaise(ref _description, value);
         }
 
         public float Price
         {
             get => _price;
-            set => _price = value;
+            set => SetAndRaise(ref _price, value);
+        }
+        public Category Category
+        {
+            get => _category;
+            set => SetAndRaise(ref _category, value);
         }
     }
 }
